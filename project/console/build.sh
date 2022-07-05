28b4c1d63b5942489979d0e122394ba72d3a9675
Akira <wulcat@icloud.com>
2022-07-05 16:58:18 +0530

    Builds the console application and links frameworks using ninja

--- project/console/build.sh
+++ project/console/build.sh
@@ -3,7 +3,13 @@
 . ../shared-scripts.sh
 
 verify_build_folder_exists
+clean_build_folder
 
+# Build using CMake
+#cmake -B build -S .
+#make -C build
+
+# Build using Ninja
 # shellcheck disable=SC2164
 pushd build
   # Request that CMake configures iteself based on what it finds in the parent folder
