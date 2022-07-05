0576f6d54b163d1a830b3465d09abc3b4dcd4035
Akira <wulcat@icloud.com>
2022-07-05 23:18:53 +0530

    Builds for web and runs successfully

---
+++ project/emscripten/cmake-post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copy our 'template.html' into the output folder as 'index.html'.
+cp template.html out/index.html
+
+# Navigate into the output folder then start a simple server and open it.
+pushd out
+#  http-server
+python3 -m http.server 1234 & open http://localhost:1234
+#    python -m SimpleHTTPServer 8000 & open http://localhost:8000
+popd
+
