dda584da015a0b71786d8190633ab299d4e3a9a1
Akira <wulcat@icloud.com>
2022-09-07 20:23:14 +0530

    Added mac os support (x84)

---
+++ project/macos/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Include the shared scripts
+. ../shared-scripts.sh
+
+#Ask homebrew to install dependencies
+fetch_brew_dependency "wget"
+fetch_brew_dependency "xcodegen"
+
+# Download SDL lib
+fetch_third_party_lib_sdl
+fetch_framework_sdl2
+
+# check to see if we have an existing symlink to our shared main c++ source folder
+if [ ! -d "Source" ]; then
+  echo "Linking 'Source' path to '..main/src'"
+  ln -s ../main/src Source
+fi
+
+# Create Xcode project
+echo "Generating Xcode project"
+xcodegen generate
+
+
