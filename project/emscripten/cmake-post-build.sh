ede1d595922db0907d1e395e156ce61143f8cf65
Akira <wulcat@icloud.com>
2022-09-21 21:00:28 +0530

    Added Support for killing pid for python server on Mac

--- project/emscripten/cmake-post-build.sh
+++ project/emscripten/cmake-post-build.sh
@@ -6,7 +6,12 @@ cp template.html out/index.html
 # Navigate into the output folder then start a simple server and open it.
 pushd out
 #  http-server
-python3  -m http.server 1243 & open http://localhost:1233
-#    python -m SimpleHTTPServer 8000 & open http://localhost:8000
+
+
+#python3  -m http.server 1233 & open http://localhost:1233
+
+lsof -nti:8001 | xargs kill -9
+python -m SimpleHTTPServer 8001 & open http://localhost:8001
+
 popd
 
