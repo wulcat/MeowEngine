1f125b7eb767e2682e3085cd907b2c6fa026c14a
Akira Mujawar <wulcat@icloud.com>
2025-01-28 10:53:26 +0530

    Updated post build settings for web app

--- installer/platforms/web/cmake-post-build.sh
+++ installer/platforms/web/cmake-post-build.sh
@@ -13,12 +13,12 @@ pushd ../../../builds/web
 #python3  -m http.server 1233 & open http://localhost:1233
 
 # For mac (kill's the old server)
-lsof -nti:8001 | xargs kill -9
+lsof -nti:8002 | xargs kill -9
 
-python -m SimpleHTTPServer 8001 #& open http://localhost:8001
+python -m SimpleHTTPServer 8002 #& open http://localhost:8001
 #sudo nc -lk -p 8001 -c 'nc localhost 8000 | sed "s|application/octet-stream|application/wasm|g"'
 #popd
-
+lsof -nti:8002 | xargs kill -9
 
 #  python -m SimpleHTTPServer 8000
 #  sudo lsof -t -i:8000 | xargs kill -9
